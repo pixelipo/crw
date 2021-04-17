4184af4febf1250f70778c303dd3a6b3e0fdaeb3
Marin Treselj <marin.treselj@forlagshuset.no>
2021-04-17 21:41:35 +0200

    Basic homepage

---
+++ src/components/homepage.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <p className='subtitle'>SHOP NOW</p>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <p className='subtitle'>SHOP NOW</p>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <p className='subtitle'>SHOP NOW</p>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <p className='subtitle'>SHOP NOW</p>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <p className='subtitle'>SHOP NOW</p>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <p className='subtitle'>SHOP NOW</p>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
