1a7965e6816c8038a1a8226740bbaf0669b1be00
Marin Treselj <marin.treselj@forlagshuset.no>
2021-04-18 14:28:05 +0200

    Create and style Header component; test using SVG as JSX

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 
@@ -10,6 +11,7 @@ import ShopPage from './pages/shop/shop';
 function App() {
     return (
         <div>
+            <Header />
             <Switch>
               <Route exact path='/' component={HomePage} />
               <Route path='/shop' component={ShopPage} />
---
+++ src/components/header/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import CrownIcon from '../icons/crown'
+
+import './header.scss'
+
+const Header = () => (
+    <div className='header'>
+        <Link className='logo-container' to='/'><CrownIcon primary='#808282' /></Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>SHOP</Link>
+            <Link className='option' to='/contact'>Contact</Link>
+        </div>
+
+    </div>
+)
+
+export default Header;
