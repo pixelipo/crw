dd320c7d6934ec9650b0ae64d70380d2ca04c2b5
Marin Treselj <marin.treselj@forlagshuset.no>
2021-04-21 21:04:43 +0200

    Add itemCount to CartDropdown; install and use reselect for memoization

--- src/components/icons/cart.jsx
+++ src/components/icons/cart.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
 
 import './cart.scss'
 
-const CartIcon = ({primary, toggleCartHidden}) => (
+const CartIcon = ({primary, toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <svg version="1.1" className="shopping-icon" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 407.45 407.45">
             <path d="M255.1 116.52c4.49 0 8.13-3.63 8.13-8.13 0-4.49-3.64-8.13-8.13-8.13H143.49c-4.49 0-8.13 3.63-8.13 8.13 0 4.5 3.64 8.13 8.13 8.13H255.1z" fill={primary}/>
@@ -14,12 +15,16 @@ const CartIcon = ({primary, toggleCartHidden}) => (
             <path d="M98.89 147.57c0 11.53 9.39 20.91 20.92 20.91 11.53 0 20.92-9.38 20.92-20.91 0-4.49-3.64-8.13-8.13-8.13s-8.13 3.63-8.13 8.13c0 2.56-2.09 4.65-4.67 4.65 -2.57 0-4.67-2.09-4.67-4.65 0-4.49-3.64-8.13-8.13-8.13S98.89 143.07 98.89 147.57z" fill={primary}/>
             <path d="M282.59 168.47c11.53 0 20.92-9.38 20.92-20.91 0-4.49-3.64-8.13-8.13-8.13 -4.49 0-8.13 3.63-8.13 8.13 0 2.56-2.09 4.65-4.67 4.65 -2.58 0-4.67-2.09-4.67-4.65 0-4.49-3.64-8.13-8.13-8.13 -4.49 0-8.13 3.63-8.13 8.13C261.67 159.09 271.06 168.47 282.59 168.47z" fill={primary}/>
         </svg>
-        <span className='cart-count' style={{color: primary}}>0</span>
+        <span className='cart-count' style={{color: primary}}>{itemCount}</span>
     </div>
 );
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
