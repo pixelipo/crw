589e83ab6ee474aac397418d4f9b3359ccd726e5
Marin Treselj <marin.treselj@forlagshuset.no>
2021-04-23 13:51:09 +0200

    Create Collection page component

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 
 import CollectionOverview from '../../components/collection-overview/collection-overview';
+import CollectionPage from '../collection/collection';
 
 
-const ShopPage = ({collections}) => (
+const ShopPage = ({match}) => (
     <div className='shop-page'>
-        <CollectionOverview />
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
     </div>
 );
 
