57197be0b1bbec53241c0820a5657b7b9ec26b1c
Marin Treselj <marin.treselj@forlagshuset.no>
2021-04-23 20:02:14 +0200

    Refactor CollectionOverview page with reducer; tweak datafiles

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -7,14 +7,22 @@ import {selectCollection} from '../../redux/shop/shop.selectors';
 
 import './collection.scss';
 
-const CollectionPage = ({match}) => (
-    <div className='collection-page'>
-        <h2>Collection Page</h2>
-    </div>
-);
+const CollectionPage = ({collection}) => {
+    const {title, items} = collection;
+    return (
+        <div className='collection-page'>
+            <h2>{title}</h2>
+            <div className='items'>
+                {items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
 });
 
-export default connect()(CollectionPage);
+export default connect(mapStateToProps)(CollectionPage);
