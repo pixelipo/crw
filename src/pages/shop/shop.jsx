5ce185dc816efb914ab44bef19e73db53276d7f6
Marin Treselj <marin.treselj@forlagshuset.no>
2021-04-23 12:43:08 +0200

    Move ShopData to a dedicated shop.reducer

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview'
+import CollectionPreview from '../../components/collection-preview/collection-preview';
 
-import ShopData from './shop.data.js'
+import {selectCollections} from '../../redux/shop/shop.selectors';
 
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({collections}) => (
+    <div className='shop-page'>
+        {collections.map(({id, ...otherCollectionProps}) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+    </div>
+);
 
-        this.state = {
-            collections: ShopData
-        }
-    }
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className='shop-page'>
-                {collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))}
-            </div>
-        );
-    }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
