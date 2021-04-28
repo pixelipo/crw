b0a92a90f8cabded5978304e618c91ca166f263a
Marin Treselj <marin@pixelipo.com>
2021-04-28 21:11:07 +0200

    Refactor Shop component with useEffect hook

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 
@@ -8,23 +8,17 @@ import CollectionOverviewContainer from '../../components/collection-overview/co
 import CollectionPageContainer from '../collection/collection.container';
 
 
-class ShopPage extends React.Component {
-    componentDidMount() {
-        const {fetchCollectionsStartAsync} = this.props;
-
+const ShopPage = ({fetchCollectionsStartAsync, match}) => {
+    useEffect(() => {
         fetchCollectionsStartAsync();
-    }
+    }, [fetchCollectionsStartAsync]);
 
-    render() {
-        const {match} = this.props;
-
-        return (
-            <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
-                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-            </div>
-        );
-    }
+    return (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+        </div>
+    );
 }
 
 const mapDispatchToProps = dispatch => ({
