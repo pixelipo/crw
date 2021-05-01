48841f318e7ac563dc56c3b3b1eea06a5fc5b282
Marin Treselj <marin@pixelipo.com>
2021-05-01 15:09:01 +0200

    Pass Stripe orders from React app via node endpoint

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51IjU1eJOz4pPyfE0Fv4QQLAlePCYtp71kiQruRCnismO4kIZua87qfXqUj86R4Zf3MzWTdOwyNDGvkJCRATcwwbi00YMH6iN44';
 
     const onToken = token => {
-        console.log(token);
-        console.log('Payment sucessful!');
-    }
+        axios({
+            url: 'http://localhost:5000/payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful');
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('Payment failed');
+        });
+    };
+
     return (
         <StripeCheckout
             label='Pay Now'
