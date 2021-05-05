1e787a519ae27bd8a9d26cb37f42512d86a6d914
Marin Treselj <marin@pixelipo.com>
2021-05-05 21:23:52 +0200

    Wrap app with Apollo

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import {
+    ApolloClient,
+    ApolloProvider,
+    InMemoryCache
+} from '@apollo/client';
 
 import {store, persistor} from './redux/store';
 import CartProvider from './providers/cart/cart.provider';
@@ -10,16 +15,22 @@ import CartProvider from './providers/cart/cart.provider';
 import './index.css';
 import App from './App';
 
+const client = new ApolloClient({
+    uri: 'https://crwn-clothing.com',
+    cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-    <CartProvider>
-        <Provider store={store}>
-            <BrowserRouter>
-                <PersistGate persistor={persistor}>
-                    <App />
-                </PersistGate>
-            </BrowserRouter>
-        </Provider>
-    </CartProvider>,
+    <ApolloProvider client={client}>
+        <CartProvider>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <PersistGate persistor={persistor}>
+                        <App />
+                    </PersistGate>
+                </BrowserRouter>
+            </Provider>
+        </CartProvider>
+    </ApolloProvider>,
     document.getElementById('root')
 );
