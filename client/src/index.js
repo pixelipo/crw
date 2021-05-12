f246d3f953a0b4ba5eadc7b9fc597715ebbfb7db
Marin Treselj <marin@pixelipo.com>
2021-05-12 12:38:21 +0200

    Add PWA compliance code

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
-import {ApolloClient, ApolloProvider} from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, ApolloProvider } from '@apollo/client';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { cache } from './graphql/cache';
 import App from './App';
 
@@ -21,3 +22,5 @@ ReactDOM.render(
     </ApolloProvider>,
     document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
